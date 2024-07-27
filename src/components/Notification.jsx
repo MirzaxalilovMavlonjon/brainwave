24861822666ef640af3a994edc0b40ed321a08e4
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-07-27 15:04:34 +0500

    "hero and notifications updated",

---
+++ src/components/Notification.jsx
@@ -0,0 +1,11 @@
+const Notification = ({ className, title }) => {
+  return (
+    <div
+      className={`${
+        className || ""
+      } flex ites-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
+    ></div>
+  );
+};
+
+export default Notification;
