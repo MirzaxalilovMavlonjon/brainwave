38619ba31bcd463110c6cf5b35109c8803235f13
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-08-07 13:41:52 +0500

    services and collaborations created and updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 const App = () => {
   return (
     <>
@@ -9,6 +11,8 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
