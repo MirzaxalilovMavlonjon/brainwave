368895a467de1d41aaceb02cdf5b7b0d5c0a0c88
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-08-08 08:06:48 +0500

    services component updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,9 @@
 import Heading from "./Heading";
 import Section from "./Section";
-import { check, service1 } from "../assets";
+import { check, service1, service2 } from "../assets";
 import { brainwaveServices } from "../constants";
+import Generating from "./Generating";
+import {PhotoChatMessage} from './design/Services'
 
 const Services = () => {
   return (
@@ -23,18 +25,43 @@ const Services = () => {
               />
             </div>
             <div className="relative z-1 max-w-[17rem] ml-auto ">
-              <h4>Smartest Ai </h4>
-              <p>Brainwave unlocks the potential of AI-powered applications</p>
+              <h4 className="h4 mb-4">Smartest AI </h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Brainwave unlocks the potential of AI-powered applications
+              </p>
               <ul className="body-2">
                 {brainwaveServices.map((item, index) => (
-                  <li key={index}>
-                    <img src={check} alt="" />
-                    <p>
-                      {item}
-                    </p>
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img src={check} width={24} height={24} alt="check" />
+                    <p className="ml-4">{item}</p>
                   </li>
                 ))}
               </ul>
+            </div>
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot"
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                Automatically enhance your photos using our AI app's photo editing feature. Try it now!
+              
+                </p>
+              </div>
             </div>
           </div>
         </div>
