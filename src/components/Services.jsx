e60d709b1c8ffcf393a11e0662e0b3d57db6f576
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-08-09 15:19:29 +0500

    services updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,14 @@
 import Heading from "./Heading";
 import Section from "./Section";
 import { check, service1, service2 } from "../assets";
-import { brainwaveServices } from "../constants";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
-import {PhotoChatMessage} from './design/Services'
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -58,9 +63,41 @@ const Services = () => {
               <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
                 <h4 className="h4 mb-4">Photo editing</h4>
                 <p className="body-2 mb-[3rem] text-n-3">
-                Automatically enhance your photos using our AI app's photo editing feature. Try it now!
-              
+                  Automatically enhance your photos using our AI app&apos;s
+                  photo editing feature. Try it now!
                 </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3 ">
+                  The world’s most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={` rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.3 bg-conic-gradient  md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }
+                      >
+                        <img src={item} alt="item" />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
